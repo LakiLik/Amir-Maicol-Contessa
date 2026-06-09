ac72bb596fa898431285b1813b77384681e130e9
LakiLik <79385660+LakiLik@users.noreply.github.com>
2026-06-09 17:44:31 +0200

    build: update application assets
    Remove temporary test assets and update project icons.

--- generate_icons.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-fs.copyFileSync('test_prod.png', 'public/logo.png');
-fs.copyFileSync('test_prod.png', 'public/icon-192x192.png');
-fs.copyFileSync('test_prod.png', 'public/icon-512x512.png');
-
